ed04fa5989ac257572810681ff776ed72d0f83c3
Lukáš Nekola <nekola.luky@seznam.cz>
2021-11-05 14:05:26 +0100

    change 7

--- du1.py
+++ du1.py
@@ -22,23 +22,27 @@ def circle_function(hor, ver):
     pendown()
     circle(4)      #tvorba kruhu pomocí existující funkce
 
-#fUNKCE URČENÍ KŘÍŽKU NA OSE X
-def coordinates_hor(value):
+#FUNKCE URČENÍ KŘÍŽKU NA OSE X
+def coordinates_hor(siteX):
+    value = int(input("Zadej buňku na vodorovné ose -> ")) #zadání hodnoty na vodorovné ose (dle úkolu je možné, aby program spadl, když se zadá číslo nekonvertovatelné na int)
     while ((value > siteX) or (value <= 0)):      #vstupní hodnota value musí být v rozsahu 1 - siteX
         if(value > siteX):
             print("Hodnota musí být stejné nebo menší než ", siteX) 
         elif(value <= 0):
             print("Hodnota musí být větší než 0")       
         value = int(input("Zadej buňku na vodorovné ose -> "))  #případná oprava hodnoty
+    return value
 
-#fUNKCE URČENÍ KŘÍŽKU NA OSE Y
-def coordinates_ver(value):
+#FUNKCE URČENÍ KŘÍŽKU NA OSE Y
+def coordinates_ver(siteY):
+    value = int(input("Zadej buňku na svislé ose -> "))  #níže princip jako u osy X (tentokrát na svislé ose)
     while ((value > siteY) or (value <= 0)):      #vstupní hodnota value musí být v rozsahu 1 - siteY
         if(value > siteY):
             print("Hodnota musí být stejné nebo menší než ", siteY) 
         elif(value <= 0):
             print("Hodnota musí být větší než 0")       
         value = int(input("Zadej buňku na vodorovné ose -> "))  #případná oprava hodnoty
+    return value
     
 #VYKRESLENÍ HRACÍHO POLE NA ZÁKLADĚ ZADANÉ VELIKOSTI
 siteX = int(input("Zadej rozměr X hracího pole -> "))
@@ -48,7 +52,8 @@ if siteX <= 1:                                           #piškvorky nejdou hrá
 siteY = int(input("Zadej rozměr Y hracího pole -> "))
 if siteY <= 1:                                           #piškvorky nejdou hrát na jednom sloupci
     print("Musíš zadat kladné číslo a větší než 1!!!")               
-    exit()                                                                                       
+    exit() 
+                                                                                          
 speed(0)   
 for m in range(siteY):       #vykresleno hrací pomocí tří vložených FOR cyklů                            
     for k in range(siteX):
@@ -68,45 +73,32 @@ print("Úspěšně jsi nakreslil hrací plochu a jdeme hrát!! Začíná hráč 
 #MEZIVÝPOČTY POTŘEBNÉ PRO SPRÁVNOU HRU
 numberSquare = siteX * siteY        #počet všceh tahů (hracích buněk)
 odEvenLap = numberSquare // 2
-print(odEvenLap)       
+#print(odEvenLap)       
 residue = numberSquare % 2
-print(residue)          #pokud bude výsledkem celé číslo, residue bude nula a pokud 1, bude počet tahů lichých a provede se poslední část kódu (podmínka if)
+#print(residue)          #pokud bude výsledkem celé číslo, residue bude nula a pokud 1, bude počet tahů lichých a provede se poslední část kódu (podmínka if)
 hideturtle()                        
 
 #SUDÝ POČET TAHŮ
 for _ in range(odEvenLap):     
     #ZNAČKA X
     print("\n HRAJE HRÁČ 1")
-    horX = int(input("Zadej buňku na vodorovné ose -> ")) #zadání hodnoty na vodorovné ose (dle úkolu je možné, aby program spadl, když se zadá číslo nekonvertovatelné na int)
-    coordinates_hor(horX)
-    verX = int(input("Zadej buňku na vertikální ose -> "))  #níže princip jako u osy X (tentokrát na vertikální ose)
-    coordinates_ver(verX)
-    hashtag_function(horX,verX)        
+    valueX = coordinates_hor(siteX)
+    valueY = coordinates_ver(siteY)
+    hashtag_function(valueX,valueY)        
 
     #ZNAČKA O
     print("\n HRAJE HRÁČ 2")       #princip stejný jako u hráče 1 (rozdíl je ve vykreslení kolečka)
-    horO = int(input("Zadej buňku na vodorovné ose -> "))
-    coordinates_hor(horO)
-    verO = int(input("Zadej buňku na vertikální ose -> "))
-    coordinates_ver(verO)
-    circle_function(horO,verO)
+    valueX = coordinates_hor(siteX)
+    valueY = coordinates_ver(siteY)
+    circle_function(valueX,valueY)
     
 #PŘI LICHÉM POČTU TAHU SE PROVEDE PŘEDCHOZÍ FOR CYKLUS + NÁSLEDUJÍCÍ ČÁST KÓDU
 if(residue == 1):  
+    #ZNAČKA X
     print("\n HRAJE HRÁČ 1")
-    horX = int(input("Zadej buňku na vodorovné ose -> ")) 
-    coordinates_hor(horX)
-    verX = int(input("Zadej buňku na vertikální ose -> "))  
-    coordinates_ver(verX)
-    hashtag_function(horX,verX) 
+    valueX = coordinates_hor(siteX)
+    valueY = coordinates_ver(siteY)
+    hashtag_function(valueX,valueY)  
 
 print("\n\nÚSPĚŠNĚ JSI DOHRÁL HRU")
 exitonclick()
-
-
-
-
-
-        
-
-        
